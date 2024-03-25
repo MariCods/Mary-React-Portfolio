c162735c945a919a20e454ccd2794e91cfa16a62
Mary Cods <maryam.selassie@gmail.com>
2024-03-25 15:47:27 -0700

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Section from "./components/Section";
