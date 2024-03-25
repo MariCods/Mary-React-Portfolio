48473001ef7f670bfd574a66f7773a4ce2eb887a
Mary Cods <maryam.selassie@gmail.com>
2024-03-24 17:39:19 -0700

    picture-change

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './styles/Works.css';
-import Image2 from '../components/styles/img/fashion.html.jpg';
+import Image2 from '../components/styles/img/pro.jpg';
 import Image3 from '../components/styles/img/Project-2-Nutruplanner.jpg';
 
 function Works() {
     return (
         <div class="flex">
             <a href="https://brian-lascuna.github.io/fashion-forecast/"><img src={Image2} id="content" alt=""></img></a>
-            <a href="https://nutriplanner-app.herokuapp.com/"><img src={Image3} id="content" alt=""></img></a>
-            <a href="https://activelife-app.herokuapp.com/"><img src={Image3} id="content" alt=""></img></a>
+            <a href="https://nutriplanner-app.herokuapp.com/"><img src={Image2} id="content" alt=""></img></a>
+            <a href="https://activelife-app.herokuapp.com/"><img src={Image2} id="content" alt=""></img></a>
         </div>
     )
 }
